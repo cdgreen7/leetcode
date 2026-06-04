28f4423f4ea51e1940a1341360610256f1a7b885
Caden Greenhalgh <144965360+cdgreen7@users.noreply.github.com>
2026-06-04 11:56:27 -0800

    Create intToRoman.py

---
+++ intToRoman.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        listOfSym = [["M", 1000], ["CM", 900], ["D", 500], ["CD", 400],
+        ["C", 100], ["XC", 90], ["L", 50], ["XL", 40],
+        ["X", 10], ["IX", 9], ["V", 5], ["IV", 4], ["I", 1]]
+        res = ""
+        for sym, val in listOfSym:
+            if num // val:
+                count = num // val
+                res += (sym * count)
+                num %= val
+        return res
+                
